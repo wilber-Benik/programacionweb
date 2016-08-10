c7559c95b8793af8ef53ecede977a46be9fc1b26
wilber-Benik <wilberforever@gmail.com>
2016-08-09 19:03:02 -0500

    Agregamos Presentacion y Comportamiento

--- hola-php.php
+++ hola-php.php
@@ -1,8 +1,43 @@
-<html>
-<head>
-<title>ejemplo hola mundo</title>
-</head>
-<body>
-<h1>Hola Mundo</h1>
-</body>
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title>hola php</title>
+			<style type="text/css">
+			/*1.-presentacion*/
+			h1{color:peru;}
+			p{color:#adcbf7;}
+			li:first-letter{font-size:2em;}
+			/*3.-cdn*/
+
+			
+			</style>
+			<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	</head>
+	<body>
+			<script type="text/javascript">
+				alert("Hola "+prompt("Ingresa tu nombre"))
+			</script>
+		<h1>Hola php</h1>
+		<p> Dias de la semana</p>
+		<a class="btn btn-success" href="http://www.google.com"> Ir a google
+		</a>
+		<ol>
+			<li> lunes</li>
+			<li> martes</li>
+			<li> miercoles</li>
+			<li> jueves</li>
+			<li> viernes</li>
+			<li> sabado</li>
+			<li> domingo</li>
+		</ol>
+						
+	</body>
 </html>
